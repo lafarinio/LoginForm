f2006573212c471e2720842112bcbb8ea7870e49
Rafał Kocoń <226467@student.pwr.edu.pl>
2020-09-18 11:30:39 +0200

    Create LoginModule; Create MainLoginComponent

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLoginComponent } from './components/main-login/main-login.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainLoginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
--- src/app/root-component/root.module.ts
+++ src/app/root-component/root.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { RootComponent } from './root.component';
+import { LoginModule } from '../modules/login/login.module';
 
 @NgModule({
   declarations: [
     RootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [RootComponent]
